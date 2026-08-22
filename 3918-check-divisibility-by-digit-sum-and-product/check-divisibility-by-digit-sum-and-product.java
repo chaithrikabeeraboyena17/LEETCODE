6bd336bf546baf715c7089171c3253bcb6b9044f
chaithrikabeeraboyena17 <chaithrikabeeraboyena@gmail.com>
2026-08-22 22:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (62.34%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=0;
         int prod=1;
         int original=n;
-      while(n>0){
+      while(n!=0){
         int digit=n%10;
            ans+=digit;
            prod*=digit;
