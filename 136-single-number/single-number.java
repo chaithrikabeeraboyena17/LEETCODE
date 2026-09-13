5942984eab19d169c3a5bce589054e2c66ea9b06
chaithrikabeeraboyena17 <chaithrikabeeraboyena@gmail.com>
2026-09-13 15:12:56 +0530

    Time: 13 ms (19.55%) | Memory: 46.8 MB (81.94%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int num:nums){
+            hs.put(num,hs.getOrDefault(num,0)+1);
+        }
+        for(int num:nums){
+            if(hs.get(num)==1){
+                return num;
+            }
+        }
+        return 0;
+    }
+}
