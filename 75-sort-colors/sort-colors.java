4ccfdf799c7b30aad6d836e69143ac5cf5a15689
chaithrikabeeraboyena17 <chaithrikabeeraboyena@gmail.com>
2026-09-14 15:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.23%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,20 +1,43 @@
 class Solution {
     public void sortColors(int[] nums) {
-      int cnt0=0,cnt1=0,cnt2=0;
-      for(int i=0;i<nums.length;i++){
-        if(nums[i]==0) cnt0++;
-        else if(nums[i]==1) cnt1++;
-        else cnt2++;
+    //   int cnt0=0,cnt1=0,cnt2=0;
+    //   for(int i=0;i<nums.length;i++){
+    //     if(nums[i]==0) cnt0++;
+    //     else if(nums[i]==1) cnt1++;
+    //     else cnt2++;
         
-      }  
-      for(int i=0;i<cnt0;i++){
-        nums[i]=0;
-      }
-       for(int i=cnt0;i<(cnt0+cnt1);i++){
-        nums[i]=1;
-      }
-       for(int i=(cnt0+cnt1);i<nums.length;i++){
-        nums[i]=2;
-      }
+    //   }  
+    //   for(int i=0;i<cnt0;i++){
+    //     nums[i]=0;
+    //   }
+    //    for(int i=cnt0;i<(cnt0+cnt1);i++){
+    //     nums[i]=1;
+    //   }
+    //    for(int i=(cnt0+cnt1);i<nums.length;i++){
+    //     nums[i]=2;
+    //   }
+
+    int low=0;
+    int mid=0;
+    int high=nums.length-1;
+     while(mid<=high){
+        if(nums[mid]==0){
+            int temp=nums[mid];
+            nums[mid]=nums[low];
+            nums[low]=temp;
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1) mid++;
+        else{
+             int temp=nums[mid];
+            nums[mid]=nums[high];
+            nums[high]=temp;
+            high--;
+        }
+
+
+        
+     }
     }
 }
