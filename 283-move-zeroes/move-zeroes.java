8b38f2405eeef1021323300bc80de8fa06e72fac
chaithrikabeeraboyena17 <chaithrikabeeraboyena@gmail.com>
2026-09-15 15:27:26 +0530

    Time: 2 ms (91.71%) | Memory: 47.7 MB (57.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-     int low=0;
-     for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[low];
-                nums[low]=temp;
-                low++;
-            }
-     }   
+    //  int low=0;
+    //  for(int i=0;i<nums.length;i++){
+    //         if(nums[i]!=0){
+    //             int temp=nums[i];
+    //             nums[i]=nums[low];
+    //             nums[low]=temp;
+    //             low++;
+    //         }
+    //  }   
+    int j=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+    for(int k=j;k<nums.length;k++){
+        nums[k]=0;
+    }
     }
 }
